ae7c4d7cf4c0017049538c839176aa6c4c3034f1
PabloSalazar333 <p.e.salazarmarquez@gmail.com>
2019-10-15 13:01:23 -0500

    Finished Bubble sort from an unordered array to an ordered array

--- src/com/exercise8bubblesort/app/BubbleSort.java
+++ src/com/exercise8bubblesort/app/BubbleSort.java
@@ -1,10 +1,54 @@
 package com.exercise8bubblesort.app;
 
+import java.util.Random;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Constants declaration
+		final int ELEMENTS_ARRAY = 100;
+		
+		//Variable declaration
+		int temp = 0;
+		
+		//Array
+		int[] unorderedArray = new int [ELEMENTS_ARRAY];
+		int[] orderedArray = new int [ELEMENTS_ARRAY];
+		
+		Random randomNumbers = new Random(System.nanoTime());
+		
+		for(int i=0; i<ELEMENTS_ARRAY; i++) 
+		{
+			unorderedArray[i] = randomNumbers.nextInt(101);
+		}
+		for(int i=0; i<ELEMENTS_ARRAY; i++)
+		{
+			System.out.println(unorderedArray[i]+" ");
+		}
+		
+		//Method bubble sort
+		for(int i=0; i<ELEMENTS_ARRAY; i++)
+		{
+			for(int j=0; j<ELEMENTS_ARRAY-1; j++)
+			{
+				if(unorderedArray[j]>unorderedArray[j+1])
+				{
+					temp = unorderedArray[j+1];
+					unorderedArray[j+1] = unorderedArray[j];
+					unorderedArray[j] = temp;
+				}
+			}
+		}
+		
+		System.out.println(" ");
+		
+		//Display the disordered vector
+		for(int i=0; i<ELEMENTS_ARRAY; i++)
+		{
+			System.out.println(unorderedArray[i]+" ");
+		}
+		
 	}
 
 }
+
